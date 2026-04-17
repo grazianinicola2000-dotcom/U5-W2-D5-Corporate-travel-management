7071b2ed861d9a716be12a3739fe5106f20d3e35
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-04-17 10:00:01 +0200

    Initial project setup with Spring Boot and PostgreSQL configuration

---
+++ src/main/java/nicolagraziani/U5_W2_D5/Corporate/travel/management/U5W2D5CorporateTravelManagementApplication.java
@@ -0,0 +1,13 @@
+package nicolagraziani.U5_W2_D5.Corporate.travel.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D5CorporateTravelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D5CorporateTravelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nicolagraziani/U5_W2_D5/Corporate/travel/management/U5W2D5CorporateTravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package nicolagraziani.U5_W2_D5.Corporate.travel.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5CorporateTravelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
